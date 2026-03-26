cef9c6225122029ed08e48fd8a5a2422ff9d7bb5
sanjaysj0302 <sanjaysj0302@gmail.com>
2026-03-26 21:00:32 +0530

    fixing the footer and header

--- react-app/src/components/Footer.jsx
+++ react-app/src/components/Footer.jsx
@@ -2,14 +2,14 @@ function Footer() {
   return (
     <footer>
       <div className="footer-inner">
-        <p className="footer-copy">&copy; 2026 Fashion Boutique. All rights reserved.</p>
 
         <div className="social-links">
           <a href="https://www.instagram.com/nila.instyle/" target="_blank" rel="noreferrer"><i className="fab fa-instagram"></i></a>
           <a href="https://www.pinterest.com/" target="_blank" rel="noreferrer"><i className="fab fa-pinterest"></i></a>
         </div>
+        <p className="footer-copy">&copy; 2026 Nila Instyle. All rights reserved.</p>
 
-        <p className="footer-credit">Website by Sanjay Palanisamy</p>
+        {/* <p className="footer-credit">Website by Sanjay Palanisamy</p> */}
       </div>
     </footer>
   );
